fc43e5b90e2087b738a2f47bcbbf15aeff11e4f5
victor.ribeiro <victor.ribeiro@codenapp.com>
2022-05-02 16:37:08 -0300

    Crud Scaffolding

---
+++ ProjetoAspNetCore/Data/ProjetoAspNetCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoAspNetCore.Models
+{
+    public class ProjetoAspNetCoreContext : DbContext
+    {
+        public ProjetoAspNetCoreContext (DbContextOptions<ProjetoAspNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoAspNetCore.Models.Department> Department { get; set; }
+    }
+}
